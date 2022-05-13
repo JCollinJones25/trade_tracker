6e3b52f38f35ce61f14aaa843be10f4fcffa086d
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-13 16:36:29 -0400

    app environment set up

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import "./App.css";
 
 function App() {
 
-  const [stocks, setStocks] = useState(null)
+  // const [stocks, setStocks] = useState(null)
 
-  const apiKey = process.env.KEY
-  const URL = `https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=${apiKey}`;
+  // const apiKey = process.env.KEY
+  // const URL = `https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=${apiKey}`;
 
-  const getStocks = () => {
-      fetch(URL)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
+  // const getStocks = () => {
+  //     fetch(URL)
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       console.log(result);
 
-        setStocks(result);
-      });
-  };
+  //       setStocks(result);
+  //     });
+  // };
 
   return (
   <div className="App">

5b92eb7c7b2bad87cf8b50b8bedb0e069c37b8df
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-16 12:44:24 -0400

    fetching data

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import "./App.css";
 import { Outlet } from 'react-router'
 
 const App = () => {
-
-  // const [stocks, setStocks] = useState(null)
-
-  // const apiKey = process.env.KEY
-  // const URL = `https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=${apiKey}`;
-
-  // const getStocks = () => {
-  //     fetch(URL)
-  //     .then((response) => response.json())
-  //     .then((result) => {
-  //       console.log(result);
-
-  //       setStocks(result);
-  //     });
-  // };
-
   return (
   <div className="App">
     <Outlet />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,50 @@
-
+import { useState, useEffect } from "react";
 
 const Main = () => {
-    return (
-        <>
-         <h1>Main</h1>
-        </>
-    )
-}
+  const [stocks, setStocks] = useState(null);
 
-export default Main
+  const apiKey = process.env.REACT_APP_API;
+    // console.log(apiKey)
+  const URL = `https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=${apiKey}`
+
+  const getStocks = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data)
+    setStocks(data.data);
+    console.log(data.data);
+  };
+
+  useEffect(() => {
+    getStocks();
+  }, []);
+
+  console.log(stocks)
+
+  const loaded = () => {
+    return (
+      <>
+        {stocks.map((stock) => {
+          return (
+            <>
+              <h1>{stock.ticker}</h1>
+              <h2>{stock.name}</h2>
+              <h3>${stock.price} / share</h3>
+              <p>Market Cap: {stock.market_cap}</p>
+              <p>Day high: {stock.day_high}</p>
+              <p>Day low: {stock.day_low}</p>
+            </>
+          );
+        })}
+      </>
+    );
+  };
+
+  const loading = () => {
+    return <h3>FETCHING DATA...</h3>;
+  };
+
+  return stocks ? loaded() : loading();
+};
+
+export default Main;

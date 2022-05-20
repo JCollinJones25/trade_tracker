379189fde405a46c390068cf0cde757e6c29ab4d
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-19 20:43:54 -0400

    took out extra comments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,26 +11,7 @@ const Home = () => {
         <h4>Your Home For Simplified Stock Market Data</h4>
         <div className="home-search"></div>
       </div>
-      {/* <h4>
-        Follow any of the links below to get started or search any stock ticker
-      </h4> */}
       <Search />
-      {/* <div className="stock-links">
-        <div className="stock-links-1">
-          <Link to="/spy">S&P 500 ETF (SPY)</Link>
-          <Link to="/aapl">Apple (AAPL)</Link>
-          <Link to="/goog">Google (GOOG)</Link>
-          <Link to="/tsla">Tesla Inc. (TSLA)</Link>
-          <Link to="/fb">META (FB)</Link>
-        </div>
-        <div className="stock-links-2">
-          <Link to="/amzn">Amazon (AMZN)</Link>
-          <Link to="/msft">Microsoft (MSFT)</Link>
-          <Link to="/mcd">McDonald's Corp (MCD)</Link>
-          <Link to="/sbux">Starbucks Corp (SBUX)</Link>
-          <Link to="/cost">Costco Wholesale (COST)</Link>
-        </div>
-      </div> */}
     </>
   );
 };

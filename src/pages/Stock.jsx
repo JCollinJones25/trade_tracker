3eb051b128fec67ab5cf258301b492686316ba25
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-09-15 00:26:40 -0400

    fix search state one step behind

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -133,9 +133,14 @@ const Stock = () => {
     return <h1>Loading date...</h1>;
   }
 
+  const handleSubmit = (newTime) => {
+    setTime(newTime);
+  }
+
   const handleClick = (newTime) => {
     setTime([]);
     setTime(newTime);
+    handleSubmit(newTime)
     console.log(newTime);
     console.log(globalData[0]);
     const price = time.map((time, idx) => ({
